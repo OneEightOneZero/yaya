ae71d450592dc765f7118f41b0e3e5ee4cf80be4
624205079 <624205079@qq.com>
2019-01-24 22:19:28 +0800

    完善购物车与后端

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -57,9 +57,9 @@
                   class="checkbox product-checkbox checked"
                   style="border-color: rgb(79, 185, 159); background: rgb(79, 185, 159);"
                 ></span>
-                <a href="javascript:;" class="product-img flex-child-noshrink">
+                <router-link :to="`/detail?guid=${i.guid}`" href="javascript:;" class="product-img flex-child-noshrink">
                   <img :src="i.imgurl" class="full-width full-height">
-                </a>
+                </router-link>
                 <div
                   class="product-info margin-left flex flex-col flex-justify-between flex-child-grow"
                 >
@@ -84,11 +84,11 @@
       <div class="recommend">
         <img src="https://img2.yaya.cn/newstatic/767/3174479e1a14d5.png" class="full-width">
         <ul class="flex flex-justify-between flex-wrap mt-5">
-            <!-- ================================推荐============================== -->
+          <!-- ================================推荐============================== -->
           <li class="recommend-item" v-for="(j,index) in tuijian" :key="index">
-            <a href="/product/68151.html">
+            <router-link :to="`/detail?guid=${j.guid}`" href="javascript:;">
               <img :src="j.imgurl">
-            </a>
+            </router-link>
             <p class="lines-1 mt-5 recommend-name" v-text="j.name"></p>
             <div class="flex flex-justify-between flex-align-center mt-5">
               <span class="red font-16" v-text="j.price"></span>
@@ -123,49 +123,59 @@
       </div>
       <div data-v-e91a6b2c></div>
     </div>
+    <Gloading/>
   </div>
 </template>
 <script>
+import Gloading from "../components/Gloading.vue";
+import { ServerUrl } from "../configs/ServerUrl";
+import $ from "jquery";
 export default {
   name: "news",
+  components: {
+    Gloading
+  },
+  methods: {
+    findorder() {
+      return new Promise(function(resolve) {
+        $.ajax({
+          type: "get", //请求方式
+          url: ServerUrl + "/goodlist/findorder", //接口路径
+          async: true, //异步
+          success: function(str) {
+            //成功回调
+            resolve(str.data);
+          }
+        });
+      });
+    },
+    getData() {
+      return new Promise(resolve => {
+        this.$.ajax({
+          type: "get",
+          url: ServerUrl + "/goodlist/type",
+          data: {
+            type: "phone"
+          },
+          success(data) {
+            resolve(data.data);
+          }
+        });
+      });
+    }
+  },
   data() {
     return {
-      orderlist: [
-        {
-          imgurl:
-            "https://img2.yaya.cn/pic/product/440x440/20180711102533799.jpg",
-          name: "华为 nova 3 （PAR-AL00） 星耀版 樱草金 6GB+128GB",
-          price: "￥2599.00"
-        },
-        {
-          imgurl:
-            "https://img2.yaya.cn/pic/product/440x440/20180711102533799.jpg",
-          name: "华为 nova 3 （PAR-AL00） 星耀版 樱草金 6GB+128GB",
-          price: "￥2599.00"
-        }
-      ],
-      tuijian:[{
-          imgurl:
-            "https://img2.yaya.cn/pic/product/440x440/20180907104359718.jpg",
-          name: "锐思 10000mAh 移动电源",
-          price: "￥99.00"
-      },{
-          imgurl:
-            "https://img2.yaya.cn/pic/product/440x440/20180907104359718.jpg",
-          name: "锐思 10000mAh 移动电源",
-          price: "￥99.00"
-      },{
-          imgurl:
-            "https://img2.yaya.cn/pic/product/440x440/20180907104359718.jpg",
-          name: "锐思 10000mAh 移动电源",
-          price: "￥99.00"
-      },{
-          imgurl:
-            "https://img2.yaya.cn/pic/product/440x440/20180907104359718.jpg",
-          name: "锐思 10000mAh 移动电源",
-          price: "￥99.00"
-      }]
+      sp: [],
+      orderlist: [],
+      tuijian: []
     };
+  },
+  async created() {
+    this.$store.commit("editLoad", true);
+    this.orderlist = await this.findorder();
+    this.tuijian = await this.getData();
+    this.tuijian,this.orderlist ? this.$store.commit("editLoad", false) : null;
   }
 };
 </script> 
