80a6f3e515623148dcc98693d7d3c5f161ca29f8
xiaogui <863684717@qq.com>
2019-01-24 14:06:54 +0800

    加载组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app" class="app-wrap">
+    <Gloading/>
     <router-view></router-view>
     <Gfooter/>
   </div>
 </template>
 
 <script>
+import Gloading from "./components/Gloading.vue"
 import Gfooter from "./components/Gfooter.vue"
 import "./assets/css/total.css"
 
 export default {
   name: "app",
   components: {
+    Gloading,
     Gfooter
   }
 };
